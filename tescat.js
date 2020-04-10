b6f2d821ae2bcae46fc81b0dc2cf29fee9d9c48e
? <zeno@attnam.com>
2020-04-10 11:03:42 +0200

    updated tescat, included the current engine here

--- tescat.js
+++ tescat.js
@@ -1241,11 +1241,11 @@ function updateGlobalBufferAndViews(buf) {
 }
 
 var STATIC_BASE = 1024,
-    STACK_BASE = 6329872,
+    STACK_BASE = 6418288,
     STACKTOP = STACK_BASE,
-    STACK_MAX = 1086992,
-    DYNAMIC_BASE = 6329872,
-    DYNAMICTOP_PTR = 1086832;
+    STACK_MAX = 1175408,
+    DYNAMIC_BASE = 6418288,
+    DYNAMICTOP_PTR = 1175248;
 
 assert(STACK_BASE % 16 === 0, 'stack must start aligned');
 assert(DYNAMIC_BASE % 16 === 0, 'heap must start aligned');
@@ -1781,7 +1781,7 @@ var tempI64;
 // === Body ===
 
 var ASM_CONSTS = {
-  1083952: function($0, $1, $2, $3) {var name = UTF8ToString($0, $1); var value = UTF8ToString($2, $3); document.getElementById(name).innerHTML = value;}
+  1172356: function($0, $1, $2, $3) {var name = UTF8ToString($0, $1); var value = UTF8ToString($2, $3); document.getElementById(name).innerHTML = value;}
 };
 
 function _emscripten_asm_const_iii(code, sigPtr, argbuf) {
@@ -1791,7 +1791,7 @@ function _emscripten_asm_const_iii(code, sigPtr, argbuf) {
 
 
 
-// STATICTOP = STATIC_BASE + 1085968;
+// STATICTOP = STATIC_BASE + 1174384;
 /* global initializers */  __ATINIT__.push({ func: function() { ___wasm_call_ctors() } });
 
 
@@ -1891,7 +1891,7 @@ function _emscripten_asm_const_iii(code, sigPtr, argbuf) {
     }
 
   function _emscripten_get_sbrk_ptr() {
-      return 1086832;
+      return 1175248;
     }
 
   function _emscripten_memcpy_big(dest, src, num) {
