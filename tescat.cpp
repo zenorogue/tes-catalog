50d51ce856518cbe2d7ada1f39d910a61c2c7178
? <zeno@attnam.com>
2022-01-06 01:58:57 +0100

    tescat.cpp: more efficient, bug in history fixed, (partial work) advanced search

--- tescat.cpp
+++ tescat.cpp
@@ -42,7 +42,7 @@ void set_value(string name, string s) {
 void set_location(string s) {
   EM_ASM_({
     var value = UTF8ToString($0, $1);
-    window.history.pushState(value, document.title + ":"+value, "?c=" + value);
+    window.history.pushState(value, document.title + ":"+value, "?c=" + encodeURIComponent(value));
     }, s.c_str(), int(s.size())
     );
   }
@@ -69,6 +69,13 @@ int numstrcmp(const char *a, const char *b) {
   else return numstrcmp(a+1, b+1);
   }
 
+string seekstr;
+
+bool at(const char *fname, const string& needle) {
+  for(char c: needle) if(*(fname++) != c) return false;
+  return true;
+  }
+
 void generate_page(string s) {
   set_location(s);
   srand(time(NULL));  
@@ -80,6 +87,9 @@ void generate_page(string s) {
   string prefix;
   curreq = ANY;
   
+  
+  // string advsearch, advsearch_str;
+  
   for(auto&[code, full, val]: restrictions) {
     if(code != "" && s.substr(0, 2) == code) {
       curreq = val;
@@ -87,20 +97,34 @@ void generate_page(string s) {
       prefix = code;
       }
     }
+  
+  /*
+  if(s.substr(0, 7) == "search/") {
+    int i = 7;
+    while(i < int(s.size()-1) && (s[i] != '/' || s[i+1] != '/')) i++;
+    advsearch = s.substr(7, i-7);
+    advsearch_str = "search/" + advsearch + "//";
+    if(i <= int(s.size() - 2))
+      s = s.substr(i+2);
+    }
+  */
 
+  while(s != "" && s[0] == '/') s = s.substr(1);
+  
   if(s != "") {
     for(int i=0; i<int(s.size())-1; i++) if(s[i] == '/') parent = s.substr(0, i+1);
-    out += genlink(prefix+parent, "go back!") + "<br/><br/>";
+    out += genlink(prefix+/*advsearch_str+*/parent, "go back!") + "<br/><br/>";
     }
   
   int len = s.size();
+  
+  vector<tesdata*> matching;
+  matching.reserve(alldata.size());
 
-  int total = 0;
   for(auto& td: alldata) {
-    string fname = td.fname;
-    if(fname.substr(0, len) != s) continue;
     if(td.type != curreq && curreq != ANY) continue;
-    total++;
+    if(at(td.fname, s)) 
+      matching.push_back(&td);
     }
 
   if(s == "") {
@@ -128,43 +152,48 @@ void generate_page(string s) {
   for(auto& [code, full, val]: restrictions) {
     if(next) out += " | ";
     next = true;
-    out += genlink(code + s, full);
+    out += genlink(code + seekstr + s, full);
     if(val == curreq) current = full;
     }
   out += " (currently showing: " + current + ")<br/><br/>";
+
+  // out += "Displaying advanced search results for: '" + advsearch + "'<br/><br/>";
+
+  out += "Advanced search: <input id=\"search\" type=\"text\"><button onClick=\"do_advanced_search('" + prefix + "', '" + s + "')\">Search!</button><br/><br/>";
     
-  out += "Number of tessellations here: " + std::to_string(total) + "<br/><br/>";    
+  out += "Number of tessellations here: " + std::to_string(matching.size()) + "<br/><br/>";    
           
   string last_explored = "?";
   
-  for(auto& td: alldata) {
-    string fname = td.fname;
-    string label = td.label;
-    string desc = td.desc;
-    if(fname.substr(0, len) != s) continue;
-    if(td.type != curreq && curreq != ANY) continue;
+  for(int ti=0; ti<(int) matching.size(); ti++) {
+    auto& td = *matching[ti];
+    const string& fname = td.fname;
+    const string& label = td.label;
+    const string& desc = td.desc;
 
     for(int i=len; i<int(fname.size()); i++) if(fname[i] == '/') {
       string where = fname.substr(0, i+1);
-      if(where != last_explored) {
+      if(true) {
         int wlen = where.size();
         map<string, pair<int, string> > samples;
         vector<string> justhere;
-        int cnt = 0;        
-        for(auto& td1: alldata) {
-          string fname1 = td1.fname;
-          if(fname1.substr(0, wlen) != where) continue;
-          if(td1.type != curreq && curreq != ANY) continue;
+        int cnt = 0;
+        
+        while(ti < (int) matching.size()) {
+          auto& td1 = *matching[ti];
+          if(!at(td1.fname, where)) break;
           cnt++;
+          const string& fname1 = td1.fname;
           for(int i=wlen; i<int(fname1.size()); i++) if(fname1[i] == '/') {
-            auto& sa = samples[fname1.substr(0, i)];
+            auto& sa = samples[fname1.substr(wlen, i-wlen)];
             sa.first++;
             if(rand() % sa.first == 0) sa.second = td1.link;
             goto next_td1;
             }
           justhere.push_back(td1.link);
-          next_td1: ;
+          next_td1: ti++;
           }
+        ti--;
         
         for(auto sa: samples) justhere.push_back(sa.second.second);
         
@@ -182,10 +211,9 @@ void generate_page(string s) {
           }
         images += "</tr></table>";
 
-        out += "<h2>" + genlink(prefix+where, where) + " (" + std::to_string(cnt) + " tessellations)</h2>";
+        out += "<h2>" + genlink(prefix+/*advsearch_str+*/where, where) + " (" + std::to_string(cnt) + " tessellations)</h2>";
         out += genlink(prefix+where, images);
         out += "<br/><br/>";
-        last_explored = where;
         }
       goto next_td;
       }
